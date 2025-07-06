57d9587ecd4c2b2e255dc8ff924ca46f8023b7b0
kunalmudgalhnd <kualmudgam@gmail.com>
2025-07-06 20:23:18 +0530

    Update setup.js

--- src/config/setup.js
+++ src/config/setup.js
@@ -1,83 +1,67 @@
 import AdminJS from "adminjs";
-import AdminJSFastify from "@adminjs/fastify"
-import * as  AdminJSMongoose from "@adminjs/mongoose";
+import AdminJSFastify from "@adminjs/fastify";
+import * as AdminJSMongoose from "@adminjs/mongoose";
 import * as Models from "../models/index.js";
 import { authenticate, COOKIE_PASSWORD, sessionStore } from "./config.js";
-import { dark, light, noSidebar } from "@adminjs/themes"
+import { dark, light, noSidebar } from "@adminjs/themes";
 
-AdminJS.registerAdapter(AdminJSMongoose);
+AdminJS.registerAdapter(AdminJSMongoose)
 
 export const admin = new AdminJS({
-    resources: [
+    resources:[
         {
             resource: Models.Customer,
             options: {
-
-                listProperties: ["name", "phone", "role", "isActivated"],
-                filterProperties: ["phone", "role", "isActivated"],
-            }
-        },
-        {
+                listProperties: ["phone", "role", "isActivated"],
+                filterProperties: ["phone", "role"],
+            },
+         },
+         {
             resource: Models.DeliveryPartner,
             options: {
-                listProperties: ["name", "email", "role", "isActivated"],
-                filterProperties: ["email", "role", "isActivated"],
-            }
-        },
-        {
+                listProperties: ["email", "role", "isActivated"],
+                filterProperties: ["email", "role"],
+            },
+         },
+         {
             resource: Models.Admin,
             options: {
-                listProperties: ["name", "email", "role", "isActivated"],
-                filterProperties: ["email", "role", "isActivated"],
-            }
-        },
-        { resource: Models.Branch },
-        { resource: Models.Category },
-
-        {
-            resource: Models.Product,
-            options: {
-                listProperties: ["name", "price", "discountPrice", "category"],
-                filterProperties: ["name", "price", "category"],
-            }
-        },
-        { resource: Models.Order ,
-            options : {
-                listProperties: ["orderId","customer","deliveryPartner","branch","items","totalPrice" ,"status"]
-            }
+                listProperties: ["email", "role", "isActivated"],
+                filterProperties: ["email", "role"],
+            },
          },
-        { resource: Models.Counter }
+        { resource: Models.Branch },
+        { resource: Models.Product },
+        { resource: Models.Category },
+        { resource: Models.Order },
+        { resource: Models.Counter },
     ],
     branding: {
-        companyName: "Satvaksha",
+        companyName: "Satvaksha:All in One",
         withMadeWithLove: false,
-        defaultTheme: dark.id,
-        availableTheme: [dark, light, noSidebar],
-        favicon : 'https://res.cloudinary.com/dkp5txigu/image/upload/v1741696157/app_icon_jra1d5.jpg',
-        
     },
-    rootPath: "/admin",
-    defaultTheme: dark.id,
-    availableThemes: [dark, light, noSidebar],
-});
+    defaultTheme:dark.id,
+    availableThemes: [dark,light,noSidebar],
+    rootPath:'/admin'
+})
 
-
-export const buildAdminRouter = async (app) => {
-    await AdminJSFastify.buildAuthenticatedRouter(admin, {
-        authenticate,
-        cookiePassword: COOKIE_PASSWORD,
-        cookieName: "admin",
-    },
-        app, {
-        store: sessionStore,
-        saveUnintialized: false,
-        resave : false,
-        secret: COOKIE_PASSWORD || 'supersecretT20',
-        cookie: {
-            httpOnly: process.env.NODE_ENV === 'production',
-            secure: process.env.NODE_ENV === 'production',
-            maxAge:1000*60*60*24,
-            sameSite: "lax" 
+export const buildAdminRouter = async(app)=>{
+    await AdminJSFastify.buildAuthenticatedRouter(
+        admin,
+        {
+            authenticate,
+            cookiePassword:COOKIE_PASSWORD,
+            cookieName:'adminjs'
+        },
+        app,
+        {
+            store:sessionStore,
+            saveUnintialized: true,
+            secret: COOKIE_PASSWORD,
+            cookie: {
+                httpOnly: process.env.NODE_ENV === "production",
+                secure: process.env.NODE_ENV === "production",
+            },
         }
-    })
+    )
 }
