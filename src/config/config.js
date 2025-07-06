97cac12c97cd2ab78520f8025b79e09ffcde45ac
kunalmudgalhnd <kualmudgam@gmail.com>
2025-07-06 20:23:54 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,28 +1,46 @@
-import "dotenv/config";
 import fastifySession from "@fastify/session";
+import ConnectMongoDBSession from "connect-mongodb-session";
+import "dotenv/config.js";
 import { Admin } from "../models/index.js";
-import connectMongodbSession from "connect-mongodb-session";
 
-const MongoDBStore = connectMongodbSession(fastifySession);
+
+export const PORT = process.env.PORT || 3001;
+export const COOKIE_PASSWORD = process.env.COOKIE_PASSWORD;
+
+const MongoDBStore = ConnectMongoDBSession(fastifySession)
 
 export const sessionStore = new MongoDBStore({
-    uri: process.env.MONGO_URI,
-    collection: "sessions",
-    databaseName : "ApnaMart"
+    uri:process.env.MONGO_URI,
+    collection:"sessions"
 })
 
-sessionStore.on("error",(error)=>{
-    console.log("Session store error: " + error);
+sessionStore.on('error',(error)=>{
+    console.log("Session store error",error)
 })
 
-export const authenticate = async (email, password) => {
-    if(email===process.env.ADMIN_EMAIL && password===process.env.ADMIN_PASSWORD){
-        return Promise.resolve({email: email, password: password});
+export const authenticate = async(email,password)=>{
+    if(email && password){
+        if(email==='kunal@gmail.com' && password==='12345678'){
+            return Promise.resolve({ email: email, password });
+        }else{
+            return null
+        }
     }
-    else{
-        return null;
-    }
+
+
+    // UNCOMMENT THIS WHEN CREATED ADMIN MANULLY
+    //if(email && password){
+    //    const user = await Admin.findOne({email});
+    //    if (!user){
+    //        return null
+    //    }
+    //    if(user.password===password){
+    //        return Promise.resolve({ email: email, password: password });
+    //    }else{
+    //        return null
+    //    }
+    //}
+
+
+    return null
 }
-
-export const PORT = process.env.PORT || 3000;
-export const COOKIE_PASSWORD = process.env.COOKIE_PASSWORD;
